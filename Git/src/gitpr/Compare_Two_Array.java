8852ea9346e459b75bc0730454e9f50bb76b210d
Sumit Salave <Sumit Salave@LAPTOP-P25KPLPS>
2021-12-21 11:12:47 +0530

    Second time commited

---
+++ Git/src/gitpr/Compare_Two_Array.java
@@ -0,0 +1,18 @@
+package gitpr;
+
+import java.util.Arrays;
+
+public class Compare_Two_Array {
+
+	public static void main(String[] args) {
+		int ar1[]= {1,2,3}; 
+		int ar2[]= {4,5,6}; 
+		int ar3[]= {1,2,3}; 
+		
+		System.out.println(Arrays.equals(ar1, ar2));
+		System.out.println(Arrays.equals(ar1, ar3));
+		
+
+	}
+
+}
